37f63b9b63f917232ca47574203d912341bd4721
ASUS <shehansandakalum2003@gmail.com>
2025-02-16 12:46:51 +0530

    Tailwindcss Add

---
+++ library-management-frontend/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className="bg-blue-50 text-center py-10">
+      <h1 className="text-4xl font-bold text-blue-500">Library Management Sysytem</h1>
+    </main>
+  );
+}
+
+export default App;
