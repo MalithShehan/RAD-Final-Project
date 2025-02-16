4d11cf0b1a239c8a3c9d82226ebe975dabffdfe1
ASUS <shehansandakalum2003@gmail.com>
2025-02-16 21:14:27 +0530

    Add a button unsin tailwindcss

--- library-management-frontend/src/App.tsx
+++ library-management-frontend/src/App.tsx
@@ -1,8 +1,9 @@
+import React from 'react';
+import { Button } from "./components/components/ui/button";
+
 function App() {
   return (
-    <main className="bg-blue-50 text-center py-10">
-      <h1 className="text-4xl font-bold text-blue-500">Library Management Sysytem</h1>
-    </main>
+    <Button className='my-4 ml-4'>Click me</Button>
   );
 }
 
